c91e38e6da1d5dfcab4064587a1ca7e6c394f52f
Aleksandra <a0993715642@gmail.com>
2021-05-01 15:15:53 +0300

    #app delete

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,15 +1,5 @@
 <template>
 <div>
-  <div id="app">
-  </div>
   <router-view />
 </div>
 </template>
-
-<script>
-export default {
-  mounted() {
-    console.log("App mounted.");
-  },
-};
-</script>
