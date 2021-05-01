67af15843b781a6506fdc8dc4d639df9d525d6bb
Aleksandra <a0993715642@gmail.com>
2021-05-02 00:54:06 +0300

    get fact from api

--- resources/js/components/FactComponent.vue
+++ resources/js/components/FactComponent.vue
@@ -2,8 +2,14 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8">
-        <div class="card">
-          <h2>Facts</h2>
+        <div class="card mt-4 p-4">
+          <h2 style="color:green; font-weight:bold">Todays new fact:</h2>
+          <div class="card" style="width: 80%; margin:auto">
+            <div class="card-body">
+                <p v-if="info" class="card-text" style="color:green; font-style:italic">"{{info}}"</p>
+                <p v-if="fact" class="card-text" style="color:green; font-style:italic">"{{fact.fact}}"</p>
+            </div>
+          </div>  
         </div>
       </div>
     </div>
@@ -12,18 +18,25 @@
 
 <script>
 export default {
-  mounted() {
-    console.log("Fact mounted.");
-    this.getFacts();
-  },
-  methods: {
-    getFacts() {
-        let path = 'http://localhost:8080/' + 'api/';
-
-        this.axios.get(path).then((response) => {
-            console.log(response.data);
-        });
+    data() {
+        return {
+            fact : {},
+            info : {}
+        }
     },
-  },
+    mounted() {
+        this.getFacts();
+    },
+    methods: {
+        //get fatc from api
+        getFacts() {
+            let path = Vue.config.API_URL + 'api/';
+
+            this.axios.get(path, ((this.fact.id) ? this.fact.id : '')).then((response) => {
+                this.fact = response.data.fact;
+                this.info = response.data.info;
+            });
+        },
+    },
 };
 </script>
