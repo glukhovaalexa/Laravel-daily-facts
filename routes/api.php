dc962894c40e058ab75939b8ef18885d3cfdb7ac
Aleksandra <a0993715642@gmail.com>
2021-05-02 19:57:28 +0300

    adit route

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,4 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::namespace('Api')->group(function () {
     Route::get('/', 'FactController@index');
-    // Route::get('/', function(){
-        // $schedule->command('fact:daily')->everyMinute();
-        // Artisan::call('schedule:run');
-    // });
-
 });
